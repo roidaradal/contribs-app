4804a105ea6d52f79d611e3b83e9badd1f0fce3c
roidaradal <roidaradal@gmail.com>
2026-02-15 21:38:06 +0800

    Add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="footer">
+        
+        <p>
+            <img class="logo" alt="logo" title="GitHub Contribs" src="@/assets/logo.png" /> | 
+            <a href="https://github.com/roidaradal">@roidaradal</a> | 
+            2026
+        </p>
+    </div>
+</template>
+
+<style scoped>
+    #footer {
+        padding-top: 1em;
+        text-align: center;
+        clear: both;
+        
+        a, a:visited, a:hover, a:active {
+            color: green;
+        }
+    }
+    img.logo {
+        height: 1.5em;
+        vertical-align: middle;
+    }
+</style>
